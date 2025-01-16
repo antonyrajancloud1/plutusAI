c4af64bcb04c697b1678f09f380544b357e7f012
antony rajan <53305623+antonyrajancloud1@users.noreply.github.com>
2025-01-16 12:47:33 +0530

    Update AngelOneAuth.py
    changed http tp request

--- plutusAI/server/broker/AngelOne/AngelOneAuth.py
+++ plutusAI/server/broker/AngelOne/AngelOneAuth.py
@@ -1,6 +1,7 @@
 import http.client
 import json
 import mimetypes
+import requests
 
 import pyotp
 
@@ -21,20 +22,53 @@ class AngelOneAuth:
             self.broker_qr = self.user_broker_details[BROKER_QR]
             self.broker_api_token = self.user_broker_details[BROKER_API_TOKEN]
             self.totp = pyotp.TOTP(self.broker_qr).now()
+            addLogDetails(INFO, "user_broker_details")
+            addLogDetails(INFO, self.user_broker_details)
             self.GenerateAuthToken()
         else:
             no_broker_data = "No Broker Details Present for Email : " + self.user_id
             print(no_broker_data)
             addLogDetails(ERROR, no_broker_data)
 
+    # def GenerateAuthToken(self):
+    #     try:
+    #         addLogDetails(INFO, "Into GenerateAuthToken")
+    #         conn = http.client.HTTPSConnection(
+    #             "apiconnect.angelbroking.com"
+    #         )
+    #         # payload = "{\n\"clientcode\":\"A58033497\",\n\"password\":\"1005\"\n,\n\"totp\":\"482435\"\n}"
+    #         payload = "{\n\"clientcode\":\"" + self.broker_user_id + "\",\n\"password\":\"" + self.broker_mpin + "\"\n,\n\"totp\":\"" + self.totp + "\"\n}"
+    #         headers = {
+    #             'Content-Type': 'application/json',
+    #             'Accept': 'application/json',
+    #             'X-UserType': 'USER',
+    #             'X-SourceID': 'WEB',
+    #             'X-ClientLocalIP': 'CLIENT_LOCAL_IP',
+    #             'X-ClientPublicIP': 'CLIENT_PUBLIC_IP',
+    #             'X-MACAddress': 'MAC_ADDRESS',
+    #             'X-PrivateKey': self.broker_api_token
+    #         }
+    #         conn.request(
+    #             "POST",
+    #             "/rest/auth/angelbroking/user/v1/loginByPassword", payload, headers)
+
+    #         res = conn.getresponse()
+    #         data = res.read()
+    #         # print(data.decode("utf-8"))
+    #         addLogDetails(INFO, json.loads(data.decode("utf-8")))
+    #         self.updateUserToken(json.loads(data.decode("utf-8")))
+    #     except Exception as e:
+    #         addLogDetails(ERROR, e)
     def GenerateAuthToken(self):
         try:
+            addLogDetails(INFO, "Into GenerateAuthToken")
 
-            conn = http.client.HTTPSConnection(
-                "apiconnect.angelbroking.com"
-            )
-            # payload = "{\n\"clientcode\":\"A58033497\",\n\"password\":\"1005\"\n,\n\"totp\":\"482435\"\n}"
-            payload = "{\n\"clientcode\":\"" + self.broker_user_id + "\",\n\"password\":\"" + self.broker_mpin + "\"\n,\n\"totp\":\"" + self.totp + "\"\n}"
+            url = "https://apiconnect.angelbroking.com/rest/auth/angelbroking/user/v1/loginByPassword"
+            payload = {
+                "clientcode": self.broker_user_id,
+                "password": self.broker_mpin,
+                "totp": self.totp
+            }
             headers = {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
@@ -45,16 +79,17 @@ class AngelOneAuth:
                 'X-MACAddress': 'MAC_ADDRESS',
                 'X-PrivateKey': self.broker_api_token
             }
-            conn.request(
-                "POST",
-                "/rest/auth/angelbroking/user/v1/loginByPassword", payload, headers)
 
-            res = conn.getresponse()
-            data = res.read()
-            # print(data.decode("utf-8"))
-            self.updateUserToken(json.loads(data.decode("utf-8")))
+            response = requests.post(url, json=payload, headers=headers)
+            addLogDetails(INFO, f"API Response: {response.json()}")
+
+            if response.status_code == 200:
+                self.updateUserToken(response.json())
+            else:
+                addLogDetails(ERROR, f"API request failed with status code {response.status_code}")
+
         except Exception as e:
-            print(e)
+            addLogDetails(ERROR, f"Error during API request: {str(e)}")
 
     def updateUserToken(self, token_data):
         try:
