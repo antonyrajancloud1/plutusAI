24f6855120a7d6a068579b38377b3598d4d5df84
antonyrajan.d <antonyrajan.d@gmail.com>
2025-12-03 15:21:51 +0530

    Log Details and HFT

--- plutusAI/tests.py
+++ plutusAI/tests.py
@@ -14,15 +14,15 @@ def tester(request):
 
     user_email = "antonyrajan.d@gmail.com"
 
-    index_data = IndexDetails.objects.filter(index_name="nifty")
+    index_data = IndexDetails.objects.filter(index_name="nifty1")
     index_group_name = get_index_group_name(index_data)
     if index_group_name == INDIAN_INDEX:
         # FlashTrade(user_email, index, INDIAN_INDEX)
         broker_obj = Broker(user_email, index_group_name).BrokerObject
-        user_data = Configuration.objects.filter(user_id=user_email, index_name="nifty")
+        user_data = Configuration.objects.filter(user_id=user_email, index_name="nifty1")
         config_dict = list(user_data.values())[0]
         print("values Generated")
         print(config_dict)
-        checker = HighFrequencyTrading(index_name="nifty", user_email=user_email, BrokerObject=broker_obj)
+        checker = HighFrequencyTrading(index_name="nifty1", user_email=user_email, BrokerObject=broker_obj)
         checker.startHFT()
     return JsonResponse({"started":"abc"}, safe=False)
